fdaec35e80fd4ae974b9055f6090e8147913f89d
PaulineClin <94557380+PaulineClin@users.noreply.github.com>
2022-01-26 15:32:56 +0100

    Add files via upload
    Add st.cache - Works !

--- virulence_complexity.py
+++ virulence_complexity.py
@@ -12,35 +12,49 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
 
-
-def app():
-
+ 
+def app():    
     
-    st.markdown("# Pathogen fitness according the virulence complexity of the genotype pathogène")
+    st.markdown("# Pathogen fitness according the virulence complexity of the genotype pathogen")
     st.markdown("## Fitness function")
     # Figure de la fitness en fonction de i :
-        
-    @st.cache(suppress_st_warning = True)    
-    def virulence_complexity(R,c):
-        R = st.slider('Transmission rate (R):', min_value=1, max_value=100, value = 20)
-        c = st.slider('Virulence cost (c):', min_value=0.0, max_value=1.0, value = 0.3)
-        i = np.arange(1, 21, 0.1)
+    
+    @st.cache(suppress_st_warning = True)  
+    def fitness(R,c): 
         Fitness = R*(1-c)**i*i
+        return Fitness
+        
+    @st.cache(suppress_st_warning = True)       
+    def Fitness_max(R,c, Fitness):
         Fitness_max = np.where(Fitness == np.amax(Fitness))
+        return int(Fitness_max[0][0])
+    
+    @st.cache(suppress_st_warning = True)      
+    def K_max(R,c,Fitness_max):
         K_max = int(np.around(i[Fitness_max]))
-        print(K_max)
-        
-        fig, ax = plt.subplots()
-        plt.plot(i, Fitness, 'k')
-        ax.set_ylim(0, np.max(Fitness+2))
-        ax.set_xlim(1, np.max(i))
-        ax.vlines(K_max, ymin=0, ymax=np.max(Fitness+2), colors = 'k', linestyles='dashed', label =' k = {} lines'.format(K_max))
-        
-        ax.xaxis.set_major_locator(MaxNLocator(integer=True))
-        
-        ax.set_xlabel('Virulence complexity k')
-        ax.set_ylabel('Pathogen Fitness')
-        ax.legend(loc = 'upper right')
-        
-        # Show the pyplot figure in the app : 
-        st.pyplot(fig)
+        return K_max
+           
+    R = st.slider('Transmission rate (R):', min_value=1, max_value=100, value = 20)
+    c = st.slider('Virulence cost (c):', min_value=0.0, max_value=1.0, value = 0.49)
+    i = np.arange(1, 21, 0.1)
+
+    path_fitness = fitness(R,c)
+    path_fitness_max = Fitness_max(R,c,path_fitness)
+    complex_max = K_max(R,c,path_fitness_max)
+    
+    #print(K_max)
+    
+    fig, ax = plt.subplots()
+    plt.plot(i, path_fitness, 'k')
+    ax.set_ylim(0, np.max(path_fitness+2))
+    ax.set_xlim(1, np.max(i))
+    ax.vlines(complex_max, ymin=0, ymax=np.max(path_fitness+2), colors = 'k', linestyles='dashed', label =' k = {} lines'.format(complex_max))
+
+    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+
+    ax.set_xlabel('Virulence complexity k')
+    ax.set_ylabel('Pathogen Fitness')
+    ax.legend(loc = 'upper right')
+    
+    # Show the pyplot figure in the app : 
+    st.pyplot(fig)
