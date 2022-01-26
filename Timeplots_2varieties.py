e10d1bed13493b52f855b641ddc79c66cc37941e
PaulineClin <94557380+PaulineClin@users.noreply.github.com>
2022-01-26 16:50:36 +0100

    Add files via upload
    Add st.cache - Works !

--- Timeplots_2varieties.py
+++ Timeplots_2varieties.py
@@ -5,49 +5,23 @@ import matplotlib.pyplot as plt
 
 
 def app():
+    
+    plt.rc('axes', labelsize=16) 
+    
     st.markdown("# Epidemiological dynamics for a mixture with n=2 varieties")
 
     # Upload the dataset and save as csv
     st.markdown("## Time plots of the model.") 
     st.write("\n")
     
-    plt.rc('axes', labelsize=16) 
+    @st.cache(suppress_st_warning = True)  
+    def parameters(R,c,rho,nu,n):
+        params = np.array([n, R, c, rho, nu])
+        return params
+        
     
-    ### Cas n = 2 variétés résistantes et priming efficace : 
-
-    # st.header("")
-    
-    # st.subheader("")
-    
-    st.latex(r''' \text{Let fixed } \nu=1 \, , \text{ which is a dimensionless parameter equal to }
-                (\gamma+\alpha)/\alpha \\
-                \text{ where } \gamma \text{ is the priming loss rate 
-                and } \alpha \, , \text{ the harvest and replanting rate.} ''')
-    
-    # Paramètre du modèle :
-    R = st.slider('Transmission rate (R):', min_value=1, max_value=100, value = 20)
-    c = st.slider('Virulence cost (c):', min_value=0.0, max_value=1.0, value = 0.49)
-    rho = st.slider(r'''Priming efficiency:''', min_value=0.0, max_value=1.0, value = 0.8)
-    nu = 1
-    n = 2
-    
-    # Encapsulation des paramètres du modèle dans une matrice  : 
-    params = np.array([n, R, c, rho, nu])
-    
-    # Modèle complet :
-    # Conditions initiales :
-    EI = np.array([0.15, 0.01, 0.15, 0.01, 0.15, 0.01])
-    
-    # Temps : 
-    t_0 = 0.0
-    t_fin = st.number_input('Temps max:', min_value=10, max_value = 1000, value= 300)
-    pas_t = 0.01
-    
-    # Définition du tspan (vecteur) via la fonction numpy :
-    tspan = np.arange(t_0, t_fin, pas_t) # création d'un vecteur avec des valeurs uniformément espacées.
-    
-    
-    # Définition du modèle complet : 
+    # Définition du modèle complet :
+    @st.cache(suppress_st_warning = True)  
     def modele_priming_cpl(etats, t, params): 
         J1, J2, G1, G2, S1, S2 = etats          # Variables d'état
         n, R, c, rho, nu = params                  # Même ordre que dans le vecteur prédéfini
@@ -60,24 +34,8 @@ def app():
                     (1-c)*R*J2*(1/2-S2-J2-G2)-(1-rho)*S2*R*((1-c)*J2+(1-c)**2*(G1+G2))-nu*S2]
         return etatdot
     
-    # Integration numerique 
-    int_priming_cpl = odeint(modele_priming_cpl, EI, tspan, args=(params,), hmax=pas_t)
-    
-    # ################################################################################################
-    # Modèle réduit :
-        
-    # Conditions initiales :
-    EI_bis = np.array([int_priming_cpl[2999,0], int_priming_cpl[2999,2], int_priming_cpl[2999,4]])
-    
-    # Temps : 
-    t_0_bis = 0.0
-    t_fin_bis = st.number_input('Temps max:', min_value=10, max_value = 1000, value= 30)
-    pas_t_bis = 0.01
-    
-    # Définition du tspan (vecteur) via la fonction numpy :
-    tspan_bis = np.arange(t_0_bis, t_fin_bis, pas_t_bis) # création d'un vecteur avec des valeurs uniformément espacées.
-    
     # Définition du modèle reduit : 
+    @st.cache(suppress_st_warning = True) 
     def modele_priming_rdt(etats, t, params): 
         x1, x2, m = etats          # Variables d'état
         n, R, c, rho, nu = params                  # Même ordre que dans le vecteur prédéfini
@@ -87,20 +45,57 @@ def app():
                     (1/n-m-x1-x2)*R*(1-c)*x1-(1-rho)*m*(R*(1-c)*x1+R*(1-c)**2*2*x2)-nu*m]
         return etatdot
     
+    @st.cache(suppress_st_warning = True) 
+    def duration(t_0,t_fin,pas_t):
+        tspan = np.arange(t_0, t_fin, pas_t)
+        return tspan
+    
+    # Paramètre du modèle :
+    R = st.slider('Transmission rate (R):', min_value=1, max_value=100, value = 20)
+    c = st.slider('Virulence cost (c):', min_value=0.0, max_value=1.0, value = 0.49)
+    rho = st.slider(r'''Priming efficiency:''', min_value=0.0, max_value=1.0, value = 0.8)
+    nu = 1
+    n = 2
+    
+    # Encapsulation des paramètres du modèle dans une matrice  : 
+    params = parameters(R,c,rho,nu,n)
+    
+    # Modèle complet :
+    # Conditions initiales :
+    EI = np.array([0.15, 0.01, 0.15, 0.01, 0.15, 0.01])
+    
+    # Temps : 
+    t_0 = 0.0
+    t_fin = st.number_input('Time max:', min_value=10, max_value = 500, value= 30)
+    pas_t = 0.01
+    
+    # Définition du tspan (vecteur) via la fonction numpy :
+    time = duration(t_0,t_fin,pas_t) # création d'un vecteur avec des valeurs uniformément espacées.
+    
     # Integration numerique 
-    int_priming_rdt = odeint(modele_priming_rdt, EI_bis, tspan_bis, args=(params,), hmax=pas_t)
+    int_priming_cpl = odeint(modele_priming_cpl, EI, time, args=(params,), hmax=pas_t)
+    
+    # ################################################################################################
+    # Modèle réduit :
+        
+    # Conditions initiales :
+    EI_bis = np.array([int_priming_cpl[2999,0], int_priming_cpl[2999,2], int_priming_cpl[2999,4]])
+    
+    
+    # Integration numerique 
+    int_priming_rdt = odeint(modele_priming_rdt, EI_bis, time, args=(params,), hmax=pas_t)
     
     # Représentation des trajectoires en fonction du temps : 
     # création d'une figure, et d'un (ou plusieurs) système d'axes
     fig, axes = plt.subplots(1,2, figsize=(15,5))
     
     # Tracer des simulations par rapport au temps pour le modèle complet : 
-    axes[0].plot(tspan, int_priming_cpl[:,0], color='blue', label='$y_1$')
-    axes[0].plot(tspan, int_priming_cpl[:,1], color='blue', linestyle='--', label='$y_2$')
-    axes[0].plot(tspan, int_priming_cpl[:,2], color='red', label='$z_1$')
-    axes[0].plot(tspan, int_priming_cpl[:,3], color='red', linestyle='--', label='$z_2$')
-    axes[0].plot(tspan, int_priming_cpl[:,4], color='green', label='$m_1$')
-    axes[0].plot(tspan, int_priming_cpl[:,5], color='green', linestyle='--', label='$m_2$')
+    axes[0].plot(time, int_priming_cpl[:,0], color='blue', label='$y_1$')
+    axes[0].plot(time, int_priming_cpl[:,1], color='blue', linestyle='--', label='$y_2$')
+    axes[0].plot(time, int_priming_cpl[:,2], color='red', label='$z_1$')
+    axes[0].plot(time, int_priming_cpl[:,3], color='red', linestyle='--', label='$z_2$')
+    axes[0].plot(time, int_priming_cpl[:,4], color='green', label='$m_1$')
+    axes[0].plot(time, int_priming_cpl[:,5], color='green', linestyle='--', label='$m_2$')
     axes[0].legend(loc='upper right') # le label est exécuter via la fonction legend
     
     # labellisation des axes
@@ -112,9 +107,9 @@ def app():
     axes[0].set_xlim(0, 30) 
     
     # Tracer des simulations par rapport au temps modèle réduit : 
-    axes[1].plot(tspan_bis, int_priming_rdt[:,0], color='blue', label='$x_1$')
-    axes[1].plot(tspan_bis, int_priming_rdt[:,1], color='red', label='$x_2$')
-    axes[1].plot(tspan_bis, int_priming_rdt[:,2], color='green', label='$m$')
+    axes[1].plot(time, int_priming_rdt[:,0], color='blue', label='$x_1$')
+    axes[1].plot(time, int_priming_rdt[:,1], color='red', label='$x_2$')
+    axes[1].plot(time, int_priming_rdt[:,2], color='green', label='$m$')
     axes[1].legend(loc='upper right') # le label est exécuter via la fonction legend
     
     # labellisation des axes
@@ -123,7 +118,7 @@ def app():
     
     # modification des bornes des axes
     axes[1].set_ylim(0, np.max(int_priming_cpl+0.05))
-    axes[1].set_xlim(0, t_fin_bis)
+    axes[1].set_xlim(0, t_fin)
 
     # Show the pyplot figure in the app : 
     st.pyplot(fig)
--- Timeplots_3varieties.py
+++ Timeplots_3varieties.py
@@ -10,47 +10,17 @@ def app():
     # Upload the dataset and save as csv
     st.markdown("## Time plots of the model.") 
     st.write("\n")
-    st.latex(r''' \text{Let fixed } \nu=1 \, , \text{ which is a dimensionless parameter equal to }
-                (\gamma+\alpha)/\alpha \\
-                \text{ where } \gamma \text{ is the priming loss rate 
-                and } \alpha \, , \text{ the harvest and replanting rate.} ''')
-    
-    
+
+
     plt.rc('axes', labelsize=16) 
     
-    # Cas n = 3 variétés résistantes et priming efficace : 
-    
-    # Paramètre du modèle :
-    R = st.slider('Transmission rate (R):', min_value=1, max_value=100, value = 20)
-    c = st.slider('Virulence cost (c):', min_value=0.0, max_value=1.0, value = 0.49)
-    rho = st.slider(r'Priming efficiency:', min_value=0.0, max_value=1.0, value=0.8)
-    nu = 1
-    n = 3
-    
-    # Encapsulation des paramètres du modèle dans une matrice  : 
-    params = np.array([n, R, c, rho, nu])
-    
-    #############################################################
-    # Modèle complet : 
-        
-        
-    # Conditions initiales :
-    EI = np.array([0.01, 0.03, 0.06,
-                    0.01, 0.06, 
-                    0.01, 0.06, 
-                    0.01, 0.06,
-                    0.01, 0.03, 0.06, 
-                    0.01, 0.03, 0.06])
-    
-    # Temps : 
-    t_0 = 0.0
-    t_fin = st.number_input('Temps max:', min_value=10, max_value = 500, value= 200)
-    pas_t = 0.01          
-    
-    # Définition du tspan (vecteur) via la fonction numpy :
-    tspan = np.arange(t_0, t_fin, pas_t) # création d'un vecteur avec des valeurs uniformément espacées.
+    @st.cache(suppress_st_warning = True)  
+    def parameters(R,c,rho,nu,n):
+        params = np.array([n, R, c, rho, nu])
+        return params
     
     # # Définition du modèle : 
+    @st.cache(suppress_st_warning = True)
     def modele_priming_cpl(etats, t, params): 
         W1, W2, W3, Y12, Y13, Y21, Y23, Y31, Y32, Z1, Z2, Z3, M1, M2, M3 = etats          # Variables d'état
         n, R, c, rho, nu = params                  # Même ordre que dans le vecteur prédéfini
@@ -72,24 +42,7 @@ def app():
                     (1-c)*R*(W1+W2)*(1/3-M3-W3-Y31-Y32-Z3)+(1-c)**2*R*(Y12+Y21)*(1/3-M3-W3-Y31-Y32-Z3)-(1-rho)*(1-c)*R*W3*M3-(1-rho)*(1-c)**2*R*(Y13+Y31)*M3-(1-rho)*(1-c)**2*R*(Y23+Y32)*M3-(1-rho)*(1-c)**3*R*(Z1+Z2+Z3)*M3-nu*M3]
         return etatdot
     
-    # Integration numerique 
-    int_priming_cpl = odeint(modele_priming_cpl, EI, tspan, args=(params,), hmax=pas_t) 
-    
-    # Modèle réduit :
-    
-    # Conditions initiales :
-    EI = np.array([int_priming_cpl[-1,1], int_priming_cpl[-1,3], int_priming_cpl[-1,9], int_priming_cpl[-1,14]])
-    # EI = np.array([0.02,0.02,0.02,0.02])
-
-    # Temps : 
-    t_0_bis = 0.0
-    #Qt_fin_bis = 50
-    pas_t_bis = 0.01
-    
-    # Définition du tspan (vecteur) via la fonction numpy :
-    tspan_bis = np.arange(t_0_bis, t_fin, pas_t_bis) # création d'un vecteur avec des valeurs uniformément espacées.
-    
-    # Définition du modèle reduit : 
+    @st.cache(suppress_st_warning = True)
     def modele_priming_rdt(etats, t, params): 
         x1, x2, x3, m = etats          # Variables d'état
         n, R, c, rho, nu = params                  # Même ordre que dans le vecteur prédéfini
@@ -101,24 +54,71 @@ def app():
                     -(1-rho)*R*m*((1-c)*x1+(1-c)**2*4*x2+(1-c)**3*3*x3)-nu*m]
         return etatdot
     
+    @st.cache(suppress_st_warning = True) 
+    def duration(t_0,t_fin,pas_t):
+        time = np.arange(t_0, t_fin, pas_t)
+        return time
+    
+    # Cas n = 3 variétés résistantes et priming efficace : 
+    
+    # Paramètre du modèle :
+    R = st.slider('Transmission rate (R):', min_value=1, max_value=100, value = 20)
+    c = st.slider('Virulence cost (c):', min_value=0.0, max_value=1.0, value = 0.49)
+    rho = st.slider(r'Priming efficiency:', min_value=0.0, max_value=1.0, value=0.8)
+    nu = 1
+    n = 3
+    
+    # Encapsulation des paramètres du modèle dans une matrice  : 
+    params = parameters(R,c,rho,nu,n)
+    
+    #############################################################
+    # Modèle complet : 
+            
+    # Conditions initiales :
+    EI = np.array([0.01, 0.03, 0.06,
+                    0.01, 0.06, 
+                    0.01, 0.06, 
+                    0.01, 0.06,
+                    0.01, 0.03, 0.06, 
+                    0.01, 0.03, 0.06])
+    
+    # Temps : 
+    t_0 = 0.0
+    t_fin = st.number_input('Time max:', min_value=10, max_value = 500, value= 200)
+    pas_t = 0.01          
+    
+    # Définition du time (vecteur) via la fonction numpy :
+    time = duration(t_0,t_fin,pas_t) # création d'un vecteur avec des valeurs uniformément espacées.
     
     # Integration numerique 
-    int_priming_rdt = odeint(modele_priming_rdt, EI, tspan_bis, args=(params,), hmax=pas_t)
+    int_priming_cpl = odeint(modele_priming_cpl, EI, time, args=(params,), hmax=pas_t) 
+    
+    # Modèle réduit :
+    
+    # Conditions initiales :
+    EI = np.array([int_priming_cpl[-1,1], int_priming_cpl[-1,3], int_priming_cpl[-1,9], int_priming_cpl[-1,14]])
+    # EI = np.array([0.02,0.02,0.02,0.02])
+
+    # Définition du modèle reduit : 
+    
+    
+    # Integration numerique 
+    int_priming_rdt = odeint(modele_priming_rdt, EI, time, args=(params,), hmax=pas_t)
     
     # Représentation des trajectoires en fonction du temps : 
     # création d'une figure, et d'un (ou plusieurs) système d'axes
     fig, axes = plt.subplots(2,2, figsize=(15,10))
     
     # Tracer des simulations par rapport au temps pour le model complet en deux fois : 
-    axes[0][0].plot(tspan, int_priming_cpl[:,0], color='blue', label='$y_1$')
-    axes[0][0].plot(tspan, int_priming_cpl[:,1], color='blue', label='$y_2$', linestyle='--')
-    axes[0][0].plot(tspan, int_priming_cpl[:,2], color='blue', label='$y_3$',linestyle='-.')
-    axes[0][0].plot(tspan, int_priming_cpl[:,9], color = 'orange', label='$w_1$')
-    axes[0][0].plot(tspan, int_priming_cpl[:,10], color = 'orange', label='$w_2$', linestyle='--')
-    axes[0][0].plot(tspan, int_priming_cpl[:,11], color = 'orange', label='$w_3$', linestyle='-.')
-    axes[0][0].plot(tspan, int_priming_cpl[:,12], color = 'green', label='$m_1$')
-    axes[0][0].plot(tspan, int_priming_cpl[:,13], color = 'green', label='$m_2$', linestyle='--',)
-    axes[0][0].plot(tspan, int_priming_cpl[:,14], color = 'green', label='$m_3$', linestyle='-.', )
+    axes[0][0].plot(time, int_priming_cpl[:,0], color='blue', label='$y_1$')
+    axes[0][0].plot(time, int_priming_cpl[:,1], color='blue', label='$y_2$', linestyle='--')
+    axes[0][0].plot(time, int_priming_cpl[:,2], color='blue', label='$y_3$',linestyle='-.')
+    axes[0][0].plot(time, int_priming_cpl[:,9], color = 'orange', label='$w_1$')
+    axes[0][0].plot(time, int_priming_cpl[:,10], color = 'orange', label='$w_2$', linestyle='--')
+    axes[0][0].plot(time, int_priming_cpl[:,11], color = 'orange', label='$w_3$', linestyle='-.')
+    axes[0][0].plot(time, int_priming_cpl[:,12], color = 'green', label='$m_1$')
+    axes[0][0].plot(time, int_priming_cpl[:,13], color = 'green', label='$m_2$', linestyle='--',)
+    axes[0][0].plot(time, int_priming_cpl[:,14], color = 'green', label='$m_3$', linestyle='-.', )
     axes[0][0].legend(loc='upper right') # le label est exécuter via la fonction legend
     
     # labellisation des axes
@@ -127,14 +127,14 @@ def app():
         
     # modification des bornes des axes
     axes[0][0].set_ylim(0, np.max(int_priming_cpl+0.05))
-    axes[0][0].set_xlim(0, np.max(tspan))   
+    axes[0][0].set_xlim(0, np.max(time))   
     
-    axes[1][0].plot(tspan, int_priming_cpl[:,3], color='darkred', label='$z_{12}$')
-    axes[1][0].plot(tspan, int_priming_cpl[:,4], color='darkred', label='$z_{13}$', linestyle='--')
-    axes[1][0].plot(tspan, int_priming_cpl[:,5], color='red', label = '$z_{21}$')
-    axes[1][0].plot(tspan, int_priming_cpl[:,6], color= 'red', label='$z_{23}$', linestyle='--')
-    axes[1][0].plot(tspan, int_priming_cpl[:,7], color='lightsalmon', label = '$z_{31}$')
-    axes[1][0].plot(tspan, int_priming_cpl[:,8], color='lightsalmon', label='$z_{32}$', linestyle='--')
+    axes[1][0].plot(time, int_priming_cpl[:,3], color='darkred', label='$z_{12}$')
+    axes[1][0].plot(time, int_priming_cpl[:,4], color='darkred', label='$z_{13}$', linestyle='--')
+    axes[1][0].plot(time, int_priming_cpl[:,5], color='red', label = '$z_{21}$')
+    axes[1][0].plot(time, int_priming_cpl[:,6], color= 'red', label='$z_{23}$', linestyle='--')
+    axes[1][0].plot(time, int_priming_cpl[:,7], color='lightsalmon', label = '$z_{31}$')
+    axes[1][0].plot(time, int_priming_cpl[:,8], color='lightsalmon', label='$z_{32}$', linestyle='--')
     axes[1][0].legend(loc='upper right') # le label est exécuter via la fonction legend
     
     # labellisation des axes
@@ -143,12 +143,12 @@ def app():
     
     # modification des bornes des axes
     axes[1][0].set_ylim(0,np.max(int_priming_cpl+0.05))
-    axes[1][0].set_xlim(0, np.max(tspan))   
+    axes[1][0].set_xlim(0, np.max(time))   
     
     # Tracer des simulations par rapport au temps pour le model réduit en deux fois :  
-    axes[0][1].plot(tspan_bis, int_priming_rdt[:,0], color='blue', label='$x_1$')
-    axes[0][1].plot(tspan_bis, int_priming_rdt[:,2], color='orange', label='$x_3$')
-    axes[0][1].plot(tspan_bis, int_priming_rdt[:,3], color='green', label='$m$')
+    axes[0][1].plot(time, int_priming_rdt[:,0], color='blue', label='$x_1$')
+    axes[0][1].plot(time, int_priming_rdt[:,2], color='orange', label='$x_3$')
+    axes[0][1].plot(time, int_priming_rdt[:,3], color='green', label='$m$')
     axes[0][1].legend(loc='upper right') # le label est exécuter via la fonction legend
     
     # labellisation des axes
@@ -157,10 +157,10 @@ def app():
 
     # modification des bornes des axes
     axes[0][1].set_ylim(0, np.max(int_priming_cpl+0.05))
-    axes[0][1].set_xlim(0, np.max(tspan_bis))   
+    axes[0][1].set_xlim(0, np.max(time))   
     
     # Tracer des simulations par rapport au temps : 
-    axes[1][1].plot(tspan_bis, int_priming_rdt[:,1], color='red', label='$x_2$')
+    axes[1][1].plot(time, int_priming_rdt[:,1], color='red', label='$x_2$')
     axes[1][1].legend(loc='upper right') # le label est exécuter via la fonction legend
     
     # labellisation des axes
@@ -169,7 +169,7 @@ def app():
     
     # modification des bornes des axes
     axes[1][1].set_ylim(0, np.max(int_priming_cpl+0.05))
-    axes[1][1].set_xlim(0, np.max(tspan_bis)) 
+    axes[1][1].set_xlim(0, np.max(time)) 
     
     # Show the pyplot figure in the app : 
     st.pyplot(fig)
