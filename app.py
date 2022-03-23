b9fef0039e6ca682e288c119be7e768e62750eef
PaulineClin <94557380+PaulineClin@users.noreply.github.com>
2022-03-23 14:29:40 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Custom imports 
 from multipage import MultiPage
-from pages import presentation, Timeplots_2varieties, Timeplots_3varieties, virulence_complexity, Prevalence, AUDPC_n_var_ode, AUDPC_transitoire_n_var_ode
+from pages import presentation, Timeplots_2varieties, Timeplots_3varieties, virulence_complexity, Prevalence, AUDPC_transitoire_n_var_ode
 
 # Create an instance of the app 
 app = MultiPage()
@@ -12,11 +12,10 @@ st.title("")
 
 # Add all your applications (pages) here
 app.add_page("Presentation", presentation.app)
-app.add_page("Time plots n=2", Timeplots_2varieties.app)
-app.add_page("Time plots n=3", Timeplots_3varieties.app)
 app.add_page("Pathogen Fitness", virulence_complexity.app)
 app.add_page("Prevalence of the disease", Prevalence.app)
-app.add_page("AUDPC of the disease at the equilibrum", AUDPC_n_var_ode.app)
+app.add_page("Plant-epidemic dynamics for n=2", Timeplots_2varieties.app)
+app.add_page("Plant-epidemic dynamics for n=3", Timeplots_3varieties.app)
 app.add_page("AUDPC of the disease over time", AUDPC_transitoire_n_var_ode.app)
 # The main app
 app.run()
